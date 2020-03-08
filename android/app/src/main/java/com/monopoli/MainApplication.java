69f83672a49f87732f22c472c63b4b2d081b0a1f
Wahyu Irwansyah <wahyu@izy.com>
2020-03-08 10:34:00 +0700

    change kusinara

--- android/app/src/main/java/com/monopoli/MainApplication.java
+++ android/app/src/main/java/com/monopoli/MainApplication.java
@@ -3,6 +3,7 @@ package com.monopoli;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.github.wumke.RNExitApp.RNExitAppPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import org.wonday.orientation.OrientationPackage;
 import com.zmxv.RNSound.RNSoundPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNExitAppPackage(),
             new RNGestureHandlerPackage(),
             new OrientationPackage(),
             new RNSoundPackage()
